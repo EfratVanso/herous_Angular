fa209c14dc14e853af0d510629fbe7196bf32a88
EfratV <EfratV@es.local>
2021-06-16 12:41:54 +0300

    use service for the data

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,15 +8,21 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;//copy, to expose the array for binding
+  constructor(private heroService: HeroService) { }
+  heroes: Hero[] = [];
 
   selectedHero?: Hero;
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();//get from the service
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-  constructor() { }
 
   ngOnInit(): void { //It's a good place to put initialization logic.
-  }
+ 
+ this.getHeroes(); }
 
 }
