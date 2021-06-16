fe93fe72814af132756a79eb990403dcb423120c
EfratV <EfratV@es.local>
2021-06-16 14:18:12 +0300

    add nav links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent }// url path and the component need to show 
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },//default rout 
+  { path: 'heroes', component: HeroesComponent },// url path and the component need to show 
+  { path: 'dashboard', component: DashboardComponent },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
