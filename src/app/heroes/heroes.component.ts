8fef4ea2f8a7b66676b5c4863322a5449bc62948
EfratV <EfratV@es.local>
2021-06-16 11:53:23 +0300

    show list of hero with *ngFor

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-   hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;//copy, to expose the array for binding
+
   constructor() { }
 
   ngOnInit(): void { //It's a good place to put initialization logic.
